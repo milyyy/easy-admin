2429ab7822e2f8d293d930521486ef4d588c61a7
milyyy <milysong@163.com>
2020-04-29 17:17:20 +0800

    跨域配置

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const dev = process.env.NODE_ENV !== 'production'
 
 function resolve(dir) {
   return path.join(__dirname, dir)
@@ -6,10 +7,19 @@ function resolve(dir) {
 
 module.exports = {
   publicPath: "./",
-  outputPath: "dist",
+  outputDir: "dist",
   assetsDir: "static",
+  lintOnSave: false,
   filenameHashing: true,
-  lintOnSave: true,
+  runtimeCompiler: true,        // 运行时版本是否需要编译
+  transpileDependencies: [],    // 默认babel-loader忽略mode_modules，这里可增加例外的依赖包名
+  productionSourceMap: false,    // 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度
+  configureWebpack: config => { // webpack配置，值位对象时会合并配置，为方法时会改写配置
+    if (dev) { // 开发环境配置
+      config.devtool = 'cheap-module-eval-source-map'
+    } else {    // 生产环境配置
+    }
+  },
   chainWebpack: config => {
     config.resolve.alias
       .set("@", resolve('src'))
@@ -19,17 +29,21 @@ module.exports = {
       .set('store', resolve('src/store'))
   },
   devServer: {
-    host: 'localhost' || '192.168.1.128',
-    port: 8080,
+    open: true,
+    host: 'localhost',
+    port: 9000,
     https: false,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1',
+        target: 'http://localhost:5000/api/', // 服务器请求接口地址
+        ws: true,
         changeOrigin: true,
         pathRewrite: {
           '^/api': ""
         }
       }
     }
-  }
+  },
+  pluginOptions: { // 第三方插件配置
+  },
 }
