f8dfc8847d69e5f640fd7c68965ccc69eb82ee12
milyyy <milysong@163.com>
2020-04-29 17:17:04 +0800

    axios封装

---
+++ client/src/config/http.js
@@ -0,0 +1,33 @@
+// 请求封装
+import axios from "axios"
+import { Message, Loading } from 'element-ui'
+
+let loading;
+function startLoading() {
+  loading = Loading.service({
+    lock: true,
+    text: '拼命加载中...',
+    background: 'rgba(0,0,0,.333)'
+  })
+}
+function closeLoading() {
+  loading.close();
+}
+
+axios.interceptors.request.use(config => {
+  startLoading();
+  return config;
+}, err => {
+  return Promise.reject(error)
+})
+
+axios.interceptors.response.use(res => {
+  closeLoading();
+  return res;
+}, err => {
+  closeLoading();
+  Message.err(err.response.data);
+  return Promise.reject(err);
+})
+
+export default axios
--- client/src/main.js
+++ client/src/main.js
@@ -4,13 +4,14 @@ import ElementUI from 'element-ui'
 
 import router from './router'
 import store from './store'
+import axios from '@/config/http'
 
-// import '@/utils/ele_config'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'assets/style/reset.css'
 import 'assets/style/main.css'
 
 Vue.use(ElementUI)
+Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false
 
